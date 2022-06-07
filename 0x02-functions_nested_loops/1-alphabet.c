3cca1226a78db8fcfd8dce23631d3a19f5ae80c7
Mohammed Mohammed A <abbaroze@gmail.com>
2022-06-07 10:31:57 -0700

    submit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print alphabet - entry point
+ * return 0
+ */
+void print_alphabet(void);
+{
+char c = 'a';
+
+while (c <= 'z')
+
+{
+_putchar(c);
+		c++;
+
+			}
+	_putchar('\n');
+}
+}
