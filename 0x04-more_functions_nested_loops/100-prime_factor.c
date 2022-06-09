e05032228d87bbe971fdf70b59f688ffbde27178
Mohammed Mohammed A <abbaroze@gmail.com>
2022-06-09 06:39:57 -0700

    submit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <math.h>
+
 /**
  * main - prime numbers
  *
  * Return: Always 0.
  */
+
 int main(void)
-
 {
-unsigned int long n = 612852475143, a = (int) sqrt(n);
+	long a, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
 
-while (1)
-{
-	if (n % a == 0)
+	for (a = 1; a <= square; a++)
 	{
-		printf("%lu \n", n / a);
-		break;
+		if (number % a == 0)
+		{
+			maxf = number / a;
+		}
 	}
-	a--;
-}
-return (0);
+
+	printf("%ld\n", maxf);
+
+	return (0);
 }
